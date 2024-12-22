d5b69c0531fde2e51db7e5abd289201bc1a0336b
Tetra <nicolas.strohl@gmail.com>
2024-12-22 14:18:53 +0100

    Optimisation des videos pour le web

--- src/tools/video.ts
+++ src/tools/video.ts
@@ -1,8 +1,8 @@
 import { getRandomInt } from "./random";
 
-import intro_plains from "../assets/videos/intro_plains_v1.mp4";
-import intro_forest from "../assets/videos/intro_forest_v2.mp4";
-import intro_submerged from "../assets/videos/intro_submerged_v1.mp4";
+import intro_plains from "../assets/videos/intro_plains_v3.mp4";
+import intro_forest from "../assets/videos/intro_forest_v5.mp4";
+import intro_submerged from "../assets/videos/intro_submerged_v3.mp4";
 
 export function getLandingPageVideo() {
     const videos = [intro_plains, intro_forest, intro_submerged];
