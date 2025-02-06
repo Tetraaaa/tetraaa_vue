82f8ae783a4eda7ca6a35b614e99da3529e5f365
Tetra <nicolas.strohl@gmail.com>
2025-02-06 21:15:39 +0100

    Retrait des listeners au unmount

--- src/pages/PianoPage.vue
+++ src/pages/PianoPage.vue
@@ -1,57 +1,67 @@
 <script setup lang="ts">
 import { isMidiNote, midiIndexToNote, NOTES, PIANO_ROLL } from '@/tools/midi';
 import { Context, loaded, now, Sampler, setContext, start } from 'tone';
+import { onMounted, onUnmounted, ref } from 'vue';
 
-navigator.requestMIDIAccess().then(midiAccess => {
-    let inx = 0
-    midiAccess.inputs.forEach((entry) => {
-        if (inx === 0) entry.onmidimessage = onMIDIMessage;
-        inx++
-    });
+let sampler: Sampler
+const midiAccess = ref<MIDIAccess>();
 
+onMounted(() => {
+    navigator.requestMIDIAccess().then(access => {
+        midiAccess.value = access
+        let inx = 0
+        access.inputs.forEach((entry) => {
+            if (inx === 0) entry.addEventListener("midimessage", onMIDIMessage)
+            inx++
+        });
+    })
+
+    setContext(new Context({ latencyHint: "playback", lookAhead: 0 }))
+
+    sampler = new Sampler({
+        urls: {
+            "A0": "A0.webm",
+            "A1": "A1.webm",
+            "A2": "A2.webm",
+            "A3": "A3.webm",
+            "A4": "A4.webm",
+            "A5": "A5.webm",
+            "A6": "A6.webm",
+            "A7": "A7.webm",
+            "C1": "C1.webm",
+            "C2": "C2.webm",
+            "C3": "C3.webm",
+            "C4": "C4.webm",
+            "C5": "C5.webm",
+            "C6": "C6.webm",
+            "C7": "C7.webm",
+            "C8": "C8.webm",
+            "D#1": "Ds1.webm",
+            "D#2": "Ds2.webm",
+            "D#3": "Ds3.webm",
+            "D#4": "Ds4.webm",
+            "D#5": "Ds5.webm",
+            "D#6": "Ds6.webm",
+            "D#7": "Ds7.webm",
+            "F#1": "Fs1.webm",
+            "F#2": "Fs2.webm",
+            "F#3": "Fs3.webm",
+            "F#4": "Fs4.webm",
+            "F#5": "Fs5.webm",
+            "F#6": "Fs6.webm",
+            "F#7": "Fs7.webm",
+        },
+        baseUrl: "audio/piano/",
+        release: 0.75,
+        onload: () => console.log("loaded")
+
+
+    }).toDestination()
 })
 
-setContext(new Context({ latencyHint: "playback", lookAhead: 0 }))
-
-const sampler = new Sampler({
-    urls: {
-        "A0": "A0.webm",
-        "A1": "A1.webm",
-        "A2": "A2.webm",
-        "A3": "A3.webm",
-        "A4": "A4.webm",
-        "A5": "A5.webm",
-        "A6": "A6.webm",
-        "A7": "A7.webm",
-        "C1": "C1.webm",
-        "C2": "C2.webm",
-        "C3": "C3.webm",
-        "C4": "C4.webm",
-        "C5": "C5.webm",
-        "C6": "C6.webm",
-        "C7": "C7.webm",
-        "C8": "C8.webm",
-        "D#1": "Ds1.webm",
-        "D#2": "Ds2.webm",
-        "D#3": "Ds3.webm",
-        "D#4": "Ds4.webm",
-        "D#5": "Ds5.webm",
-        "D#6": "Ds6.webm",
-        "D#7": "Ds7.webm",
-        "F#1": "Fs1.webm",
-        "F#2": "Fs2.webm",
-        "F#3": "Fs3.webm",
-        "F#4": "Fs4.webm",
-        "F#5": "Fs5.webm",
-        "F#6": "Fs6.webm",
-        "F#7": "Fs7.webm",
-    },
-    baseUrl: "audio/piano/",
-    release: 0.75,
-    onload: () => console.log("loaded")
-
-
-}).toDestination()
+onUnmounted(() => {
+    midiAccess.value?.inputs.forEach(entry => entry.removeEventListener("midimessage", onMIDIMessage))
+})
 
 function onMIDIMessage(event: MIDIMessageEvent) {
     if (!event.data) return
